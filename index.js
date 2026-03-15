c03f6ce439b568ac261158a2b7cd1ca3f287118b
Oswaldo Castelano <cobastelano@outlook.com>
2026-03-14 22:09:21 -0600

    Clase de pokedex.json

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json');
 
-console.log('hola mundo');
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send('Bienvenido al servidor');
+    res.send("Bienvendido al Pokedex");
 });
 
-app.get("/:name", (req,res, next) =>{
+app.get("/pokemon/all", (req,res, next) =>{
     res.status(200);
-    req.params.name; 
-    res.send("Estas en la pagina 'nombre' ");
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req,res,next) => {
+    //validar el numero maximo de pokemones para regresar datos validos
+    const id = req.params.id -1;
+    if (id >= 0 && id <=150){
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    }
+    else{
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for(i = 0; i < pokemon.length; i++){
+        if (pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
