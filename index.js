b9558032ec98198c17b5124b106c31a5135badd3
Oswaldo Castelano <cobastelano@outlook.com>
2026-03-14 19:12:09 -0600

    Recuperación de avances tras cambio de sistema

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ console.log('hola mundo');
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send('Hola mundo y Bienvenido');
+    res.send('Bienvenido al servidor');
 });
 
-app.listen(3000, () => {
+app.get("/:name", (req,res, next) =>{
+    res.status(200);
+    req.params.name; 
+    res.send("Estas en la pagina 'nombre' ");
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
 });
