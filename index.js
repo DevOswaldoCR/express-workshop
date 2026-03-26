11502f55e2567a309119d9e968a9e44d9789fe48
Oswaldo Castelano <cobastelano@outlook.com>
2026-03-25 19:38:42 -0600

    Actividad 7 Middleware para 404

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const bodyparser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const pokemon = require('./routes/pokemon')
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended : true}));
 
 app.get("/", (req, res, next) => {
-    return res.status(200).send("Bienvendido al Pokedex");
+    return res.status(200).json({code : 1 , message : "Bienvenido al pokedex"});
 });
 
 
 app.use("/pokemon",pokemon );
+
+app.use((req, res, next) => {
+    return res.status(400).json({code : 404, message: "URL no encontrada"});
+});
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
